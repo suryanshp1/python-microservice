47cba316d8e918338a19a1bd13266767fa784c7e
Suryansh Pandey <suryanshp1@gmail.com>
2023-06-03 23:32:51 +0530

    fixed linting

--- test_main.py
+++ test_main.py
@@ -1,17 +1,16 @@
-from fastapi import FastAPI
 from fastapi.testclient import TestClient
 from main import app
 
 client = TestClient(app)
 
 
-def test_read_main():
+def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Wikipedia API. Call /search or /wiki"}
 
 
-def test_read_main():
+def test_read_phrase():
     response = client.get("/phrase/Barack Obama")
     assert response.status_code == 200
     assert response.json() == {
