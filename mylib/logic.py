dc8d69f904193ec3f0862e60275e50da2d8d966a
Suryansh Pandey <suryanshp1@gmail.com>
2023-06-03 15:46:38 +0530

    adding a new cli functionality

--- mylib/logic.py
+++ mylib/logic.py
@@ -5,3 +5,8 @@ def wiki(name="War Goddess", length=1):
     """This is a wikipedia fetcher"""
 
     return wikipedia.summary(name, length)
+
+def search_wiki(name):
+    """Search wikipedia for names"""
+
+    return wikipedia.search(name)
